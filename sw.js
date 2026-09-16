08d8c291f0144282dd8ca20b7174ace157a80198
Uonsaskani <139602070+Uonsaskani@users.noreply.github.com>
2026-09-16 01:25:07 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "qhifz-v3-bomb";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/data.js",
+  "./js/lessons.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
